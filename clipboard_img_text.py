62fa573e044a2b7017dd05ffba96d4e07f129ed5
Joel Renjith <joelrenjith9@gmail.com>
2023-04-02 22:12:12 +0530

    No console view+ toaster notification [Windows 10]

--- clipboard_img_text.py
+++ clipboard_img_text.py
@@ -1,9 +1,12 @@
+#! C:\\Users\\joelr\\AppData\\Local\\Programs\\Python\\Python310\\pythonw.exe
 from PIL import Image,ImageGrab
 from pytesseract import pytesseract
 import pyperclip
-
+from win10toast import ToastNotifier
+import time
 try:
     path_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    toast = ToastNotifier()
     img = ImageGrab.grabclipboard()
     if(isinstance(img, Image.Image)):    
         pytesseract.tesseract_cmd = path_tesseract
@@ -12,12 +15,18 @@ try:
         try:
             pyperclip.copy(l)
             print("copied to clipboard")
+            toast.show_toast("Clipboard_img_text","Copied to clipboard")
+
         except:
             print("couldnt copy to clipboard")
+            toast.show_toast("Clipboard_img_text","Couldnt copy to clipboard")
 
     else:
         print("no image found in clipboard :(")
+        toast.show_toast("Clipboard_img_text","no image found in clipboard :(")
 except Exception as e:
     print(e)
-if(input()=='exit'):
-    quit
+    toast.show_toast("Clipboard_img_text","exception occured")
+    
+time.sleep(3)
+quit()
